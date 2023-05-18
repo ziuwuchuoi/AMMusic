90386074301874f69f704520614b913c9b6e6525
BICHHANG\hang <21522041@gm.uit.edu.vn>
2023-05-19 01:28:01 +0700

    sign in+ sign up + reset password

--- scr/assets/icons/index.js
+++ scr/assets/icons/index.js
@@ -1,1 +1,4 @@
 export {default as IC_BACK} from './Back.png';
+export {default as IC_GOOGLE} from './google.png';
+export {default as IC_FACEBOOK} from './facebook.png';
+export {default as IC_NEXT} from './next.png';
