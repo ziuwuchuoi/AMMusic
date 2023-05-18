30e93ba5d52a3f6050d8741aebd8b3ca1763ea7c
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2023-05-18 23:38:55 +0700

    he

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { SafeAreaView, View, Text } from "react-native";
 import LoadingScreen from "./screens/LoadingScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import UploadScreen from "./screens/UploadScreen";
 
 
 const App = () => {
   return (
     <SafeAreaView>
-      <WelcomeScreen/>
+      <UploadScreen/>
     </SafeAreaView>
   );
 };
--- scr/assets/icons/index.js
+++ scr/assets/icons/index.js
@@ -1,1 +1,2 @@
 export {default as IC_BACK} from './Back.png';
+export {default as IC_UPLOAD} from './Upload.png';
