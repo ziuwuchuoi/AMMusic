2f8e39297bafbaf4898b13aeedd658be1e5f46cb
BICHHANG\hang <21522041@gm.uit.edu.vn>
2023-08-10 15:14:29 +0700

    load model in app

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,61 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Metro configuration for React Native
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+// };
+
+// const { getDefaultConfig } = require('metro-config');
+// module.exports = (async () => {
+//   const defaultConfig = await getDefaultConfig();
+//   const { assetExts } = defaultConfig.resolver;
+//   return {
+//     resolver: {
+//       // Add bin to assetExts
+//       assetExts: [...assetExts, 'bin'],
+//     }
+//   };
+// })();
+
+// // const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// // /**
+// //  * Metro configuration
+// //  * https://facebook.github.io/metro/docs/configuration
+// //  *
+// //  * @type {import('metro-config').MetroConfig}
+// //  */
+// // const config = {};
+
+// // module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig();
+  const { assetExts } = defaultConfig.resolver;
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      // Add bin to assetExts
+      assetExts: [...assetExts, 'bin'],
+    }
+  };
+})();
